381d14d34340244d157d97e79372933db15dc6f6
txrunishere <tsoni9286@gmail.com>
2025-01-19 11:11:08 +0530

    add login and logout route and set up authorization

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -1,9 +1,26 @@
-const { asyncHandler } = require("../utils/asyncHandler");
+const { asyncHandler } = require("../utils/asyncHandler.js");
 const { ApiError } = require("../utils/ApiError.js");
 const { ApiResponse } = require("../utils/ApiResponse.js");
 const { uploadOnCloudinary } = require("../utils/cloudinary.js");
 const User = require("../models/user.model.js"); // we import it here because it directly connect with the mongoose / mongoDB
 
+// method for generate access and refresh token
+const generateAccessAndRefreshToken = async (userId) => {
+  try {
+    const user = await User.findById(userId);
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({ validateBeforeSave: false });
+
+    return { refreshToken, accessToken };
+  } catch (error) {
+    throw new ApiError(500, "refresh and access token are not generated!!");
+  }
+};
+
+// Register user
 const registerUser = asyncHandler(async (req, res) => {
   const { fullName, email, password, username } = req.body;
 
@@ -30,7 +47,12 @@ const registerUser = asyncHandler(async (req, res) => {
 
   let coverImageLocalPath;
 
-  if (req.files && Array.isArray(req.files.coverImage) && req.files.coverImage.length > 0) coverImageLocalPath = req.files.coverImage[0].path 
+  if (
+    req.files &&
+    Array.isArray(req.files.coverImage) &&
+    req.files.coverImage.length > 0
+  )
+    coverImageLocalPath = req.files.coverImage[0].path;
 
   if (!avatarLocalPath) throw new ApiError(400, "Avatar file is required!!");
 
@@ -63,6 +85,80 @@ const registerUser = asyncHandler(async (req, res) => {
     .json(new ApiResponse(200, checkUser, "User registered successfully!!"));
 });
 
+// Login user
+const loginUser = asyncHandler(async (req, res) => {
+  const { username, email, password } = req.body;
+
+  // if (!(username || email))
+  // if (!username && !email)
+  if (!username && !email)
+    throw new ApiError(400, "Email or Username is required!!");
+
+  const checkUser = await User.findOne({
+    $or: [{ username }, { email }],
+  });
+
+  if (!checkUser) throw new ApiError(404, "User not Exists");
+
+  const checkPasswordValidation = await checkUser.isPasswordCorrect(password);
+  if (!checkPasswordValidation) throw new ApiError(401, "Invalid Password!!");
+
+  const { refreshToken, accessToken } = await generateAccessAndRefreshToken(
+    checkUser._id
+  );
+
+  const checkLoggedInUser = await User.findById(checkUser._id).select(
+    "-password -refreshToken"
+  );
+
+  const cookieOptions = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .cookie("accessToken", accessToken, cookieOptions)
+    .cookie("refreshToken", refreshToken, cookieOptions)
+    .json(
+      new ApiResponse(
+        200,
+        {
+          user: checkLoggedInUser,
+          accessToken,
+          refreshToken,
+        },
+        "User logged in"
+      )
+    );
+});
+
+const logoutUser = asyncHandler(async (req, res) => {
+  await User.findByIdAndUpdate(
+    req.user._id,
+    {
+      $set: {
+        refreshToken: undefined,
+      },
+    },
+    {
+      new: true,
+    }
+  );
+  const cookieOptions = {
+    httpOnly: true,
+    secure: true,
+  };
+
+  return res
+    .status(200)
+    .clearCookie("accessToken", cookieOptions)
+    .clearCookie("refreshToken", cookieOptions)
+    .json(new ApiResponse(200, {}, "User logout"));
+});
+
 module.exports = {
   registerUser,
+  loginUser,
+  logoutUser,
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,14 +1,20 @@
 const { Router } = require("express");
-const { registerUser } = require("../controllers/user.js");
+const {
+  registerUser,
+  loginUser,
+  logoutUser,
+} = require("../controllers/user.js");
+const verifyJWT = require("../middlewares/auth.middleware.js");
 const upload = require("../middlewares/multer.middleware.js");
 
 const router = Router();
 
 router.route("/register").post(
-  upload.fields([ // a middleware to upload file images on this route
+  upload.fields([
+    // a middleware to upload file images on this route
     {
       name: "avatar",
-      maxCount: 1
+      maxCount: 1,
     },
     {
       name: "coverImage",
@@ -17,5 +23,10 @@ router.route("/register").post(
   ]),
   registerUser
 );
+
+router.route("/login").post(loginUser);
+
+// secured routes
+router.route("/logout").post(verifyJWT, logoutUser);
 
 module.exports = router;
